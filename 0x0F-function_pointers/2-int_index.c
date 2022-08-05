e238f11d00099250577e7bfe6d8eccc4a016b765
MwachiraD <denochira78@gmail.com>
2022-08-05 06:46:07 -0700

    hotfix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,31 @@
-#ointer
-* @size: size of array
-* @cmp: pointer to a function
-* Return: check for success
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+* int_index- searches for an int
+* @array: array to search for
+* @size: size of the array
+* @cmp: pointer to function that checks for the int
+* Return: retrn -1 if no int or if int is <= 0, or index
+* of int if int > 0
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i = 0;
-
-if (size > 0)
+int i, check;
+if (size <= 1 || cmp == NULL || array == NULL)
 {
-if (array != NULL && cmp != NULL)
-{
-while (i < size)
-{
-if (cmp(array[i]))
-return (i);
-i++;
-}
-}
-}
-
 return (-1);
+}
+for (i = 0; i < size; i++)
+{
+check = cmp(array[i]);
+if (check != 0)
+{
+break;
+}
+}
+if (check == 0)
+{
+return (-1);
+}
+return (i);
 }
