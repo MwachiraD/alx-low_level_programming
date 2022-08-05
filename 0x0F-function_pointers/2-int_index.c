f97155420c3e1663ba07c2f3ea55b9a097c84e0b
MwachiraD <denochira78@gmail.com>
2022-08-05 06:41:28 -0700

    hotfix

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#ointer
+* @size: size of array
+* @cmp: pointer to a function
+* Return: check for success
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+while (i < size)
+{
+if (cmp(array[i]))
+return (i);
+i++;
+}
+}
+}
+
+return (-1);
+}
